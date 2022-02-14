bee334659decf6ad9782a1ea1ab3df47e568bcd8
mayli <mayli@LAPTOP-2VTINRTE>
2022-02-14 14:53:07 -0800

    Hello World Commit

---
+++ Hello World/src/Hellojava.java
@@ -0,0 +1,18 @@
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		
+     //calling method
+		
+		greeting();
+	}
+	
+	//creating a method
+	static void greeting() {
+		System.out.println("Hello, Every one!");
+		System.out.println("I am Kyi Lei Aye who enjoys coding.");
+
+	}
+
+}
